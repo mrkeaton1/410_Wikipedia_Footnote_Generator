03632411f3ec46021bc857847ece0a071999efb9
Greg Murray <murray.greg.c@gmail.com>
2019-11-18 10:42:44 -0500

    added new fonts and some source text to play with. Issues remain with page links and of course, the system is dumb. Better language processing is needed to fix that.

--- format_pdf.py
+++ format_pdf.py
@@ -1,10 +1,15 @@
 from fpdf import FPDF, HTMLMixin
 
 class PDF(FPDF, HTMLMixin):
-        
+    def __init__(self):
+        FPDF.__init__(self)
+        HTMLMixin.__init__(self)
+        self.add_font('DejaVu', '', 'DejaVuSerif.ttf', uni=True)
+        self.add_font('DejaVu', 'B', 'DejaVuSerif-Bold.ttf', uni=True)
+        self.add_font('DejaVu', 'I', 'DejaVuSerif-Italic.ttf', uni=True)
     def header(self):
         # Arial bold 15
-        self.set_font('Arial', 'B', 15)
+        self.set_font('DejaVu', 'B', 15)
         # Calculate width of title and position
         w = self.get_string_width(self.title) + 6
         self.set_x((210 - w) / 2)
@@ -21,7 +26,7 @@ class PDF(FPDF, HTMLMixin):
         # Position at 1.5 cm from bottom
         self.set_y(-15)
         # Arial italic 8
-        self.set_font('Arial', 'I', 8)
+        self.set_font('DejaVu', 'I', 8)
         # Text color in gray
         self.set_text_color(128)
         # Page number
@@ -30,7 +35,7 @@ class PDF(FPDF, HTMLMixin):
     def text_body(self, all_words, key_words):
         link_list = []
         # Times 12
-        self.set_font('Times', '', 12)
+        self.set_font('DejaVu', '', 12)
         
         # Output text, bolding the keywords, and creating a link.
         for value, word in enumerate(all_words):
@@ -39,7 +44,7 @@ class PDF(FPDF, HTMLMixin):
                 self.ln()
                 
             if value in key_words.values():
-                self.set_font('Times','B', 12)
+                self.set_font('DejaVu','B', 12)
                 
                 #create the link...
                 link = self.add_link()
@@ -77,7 +82,7 @@ class PDF(FPDF, HTMLMixin):
             
             #print the entry word.
             self.ln()
-            self.set_font('Times','I',12)
+            self.set_font('DejaVu','I',12)
             self.cell(0,5,word,ln=1)
             
             #print the first sentence entry of the word.
@@ -85,7 +90,7 @@ class PDF(FPDF, HTMLMixin):
             self.cell(0,5,sentence, ln=1)
             
             #print the hyperlink to the wikipedia page.
-            self.set_font('Times','U',12)
+            self.set_font('DejaVu','U',12)
             self.write_html("""<A href="{0}">{0}</A>\n""".format(hyperlink))
 
     def print_document(self, all_words, key_words, notes):
